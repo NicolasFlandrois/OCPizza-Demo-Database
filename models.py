19c161d5f40b6d88c8bd61712de4fa46168b1fbb
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-07-10 22:29:35 +0200

    Initial lay out that fit the project, ready to modify

--- models.py
+++ models.py
@@ -3,7 +3,7 @@
 # Date: 
 # Author: Nicolas Flandrois
 
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, Boolean, ForeignKey
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -14,22 +14,65 @@ session = connect()
 Base = declarative_base()
 
 
-class Product(Base):
-    """docstring for Products"""
+class Pizza(Base):
+    """docstring for Pizza"""
     __tablename__ = "product"
     id = Column(Integer, primary_key=True)
-    ean = Column(String(13), nullable=False)
     name = Column(String(50))
-    category = Column(Integer, ForeignKey('category.id'))
-    substitute = Column(Integer, ForeignKey('product.id'))
-    substituted = Column(Boolean)
+    price = Column(Float(scale=2))
+    
 
-
-class Category(Base):
-    """docstring for Categories"""
-    __tablename__ = "category"
+class Ingredients(Base):
+    """docstring for Ingredient"""
+    __tablename__ = "ingredient"
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
+    ingredient = Column(String(50))
 
-    def __repr__(self):
-        return str((self.name))
+class Recipe(Base):
+    """docstring for Recipe"""
+    __tablename__ = "recipe"
+    id = Column(Integer, primary_key=True)
+    pizza = Column(Integer, ForeignKey('pizza.id'))
+    ingredient = Column(Integer, ForeignKey('ingredient.id'))
+
+class Units(Base):
+    """docstring for Units Quantity Units Delivered/Stored/Used"""
+    __tablename__ = "units"
+    id = Column(Integer, primary_key=True)
+
+class Stock(Base):
+    """docstring for Stock"""
+    __tablename__ = "stock"
+    id = Column(Integer, primary_key=True)
+
+class Payementstatus(Base):
+    """docstring for PayementStatus"""
+    __tablename__ = "payementstatus"
+    id = Column(Integer, primary_key=True)
+
+class Orderstatus(Base):
+    """docstring for Order Status"""
+    __tablename__ = "orderstatus"
+    id = Column(Integer, primary_key=True)
+
+class Client(Base):
+    """docstring for Client"""
+    __tablename__ = "Client"
+    id = Column(Integer, primary_key=True)
+
+class Vat(Base):
+    """docstring for Vat"""
+    __tablename__ = "Vat"
+    id = Column(Integer, primary_key=True)
+
+class Orders(Base):
+    """docstring for Orders"""
+    __tablename__ = "Orders"
+    id = Column(Integer, primary_key=True)
+    date = Column(Datetime()) # Using SQLALCHEMY Date or Datetime?
+    client = Column(Integer, ForeignKey('client.id'))
+    pizza = Column(Integer, ForeignKey('pizza.id'))
+    vat = Column(Integer, ForeignKey('vat.id'))
+    totalpriceht = Column(Integer, ForeignKey('pizza.price'))
+    orderstatus = Column(Integer, ForeignKey('orderstatus.id'))
+    paymentstatus = Column(Integer, ForeignKey('payementstatus.id'))
--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ from sqlalchemy import create_engine, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, query
 from sqlalchemy_utils import create_database, database_exists
-from models import Product, Category
+from models import xxxxxxxxxxxxxxxx
+from connection import connect
 from datetime import datetime
 
 
@@ -25,27 +26,21 @@ password = config["password"]
 host = config["host"]
 port = config["port"]
 
-if not database_exists(f'mysql+pymysql://{username}:{password}@{host}/off1'):
-    create_database(f'mysql+pymysql://{username}:{password}@{host}/off1')
+if not database_exists(f'mysql+pymysql://{username}:{password}@{host}/ocpizza'):
+    create_database(f'mysql+pymysql://{username}:{password}@{host}/ocpizza')
 
     # 2/connect to database: off1
     Base = declarative_base()
-    engine = create_engine(
-        f'mysql+pymysql://{username}:{password}@{host}/off1?host={host}?port=\
-        {port}', echo=False, encoding='utf8', pool_recycle=60000,
-        pool_pre_ping=True)
+    engine = connect()
 
     # 3/ Create tables in DB, named: category & product
-    metadata = MetaData(engine)
+    metadata = MetaData(engine) # Create and factor this in Connect function
 
-    product = Table(
+    pizza = Table(
         'product', metadata,
         Column('id', Integer, primary_key=True),
-        Column('ean', String(13), nullable=False),
         Column('name', String(50)),
-        Column('category', Integer, ForeignKey('category.id')),
-        Column('substitute', Integer, ForeignKey('product.id')),
-        Column('substituted', Boolean),
+        Column('price', Float(scale=2)),
         )
 
     category = Table(
@@ -55,65 +50,26 @@ if not database_exists(f'mysql+pymysql://{username}:{password}@{host}/off1'):
         )
 
     # 5/ creat all tables
-    metadata.create_all(engine)
+    metadata.create_all(engine) # Create and factor this in Connect function
 
     # 4/ Fill in info to database according to tables, with seed data.
     # 4.1/ Fill Category table with category data.
-    categories = ("pâte à tartiner", "confiture", "sirop")
-    for i in categories:
-        engine.execute(category.insert(), name=i)
-    # 4.2/Fill Product table with Products data.
-    prods = [("3017620429484", "Nutella - Ferrero", 1, 2, False),
-             ("3560070472888", "Pâte à tartiner - Carrefour Bio", 1, 1, True),
-             ("5410126006957", "The original speculoos - Lotus", 1, 4, False),
-             ("3700774300487", "Pâte à tartiner Speculos - Speculoos ", 1, 3,
-              True),
-             ("3760091721747", "Beurre de cacahuètes - Ethiquable", 1, 6,
-              False),
-             ("8710795630918", "peanut butter - Jack Klijn", 1, 5, True),
-             ("3045320001525", "Confiture Extra Fraises - Bonne Maman", 2, 8,
-              False),
-             ("3560070986897", "Confiture de fraises - Reflets de France", 2,
-              7, True),
-             ("3608580750031", "Confiture à L\'Abricot - Bonne Maman", 2, 10,
-              False),
-             ("3245390034830", "Confiture d\'abricots - Reflets de France", 2,
-              9, True),
-             ("3045320001648", "Confiture orange - Bonne Maman", 2, 12,
-              False),
-             ("3245390060709", "Confiture d\'oranges - Reflets de France", 2,
-              11, True),
-             ("2001111060035", "Coulis de fraises - Atelier Des Gouts Sucres",
-              3, 14, False),
-             ("3228170819506", "Coulis Fraises - Ponthier", 3, 13, True),
-             ("3088542500278", "Pur Sirop d\'érable - Maple joe",
-              3, 16, False),
-             ("0815126002179", "Sirop D\'érable Pur 100% - Nokomis", 3, 15,
-              True),
-             ("3088545004001", "Miel de fleurs - Lune de Miel", 3, 18, False),
-             ("3088540202860", "Miel du Poitou-Charente - Miel l\'Apiculteur",
-              3, 17, True)]
+    # categories = ("pâte à tartiner", "confiture", "sirop")
+    # for i in categories:
+    #     engine.execute(category.insert(), name=i)
+    
+    # import all raw data from json Files
 
     # 4.3/ Parsing and inserting all data.
-    for index, (
-      ean, name, category, substitute, substituted) in enumerate(prods):
-        engine.execute(product.insert(), ean=ean, name=name,
-                       category=category, substituted=substituted)
+    # for index, (
+    #   ean, name, category, substitute, substituted) in enumerate(prods):
+    #     engine.execute(product.insert(), ean=ean, name=name,
+    #                    category=category, substituted=substituted)
 
     # 5/ Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine) # Create and factor this in Connect function
     # 6/ Create a Session
-    session = Session()
-
-    # 7/ Updating info that couldn't be loaded during first run
-    # (e.g. substitute id)
-    products = session.query(Product).all()
-    for n in range(0, len(products), 2):
-        prod = products[n]
-        sub = products[n+1]
-        prod.substitute = sub.id
-        sub.substitute = prod.id
-    session.commit()
+    session = Session() # Create and factor this in Connect function
 
     # 8/ Mesuring time to setup, and informing user, the database was created
     # successfully.
@@ -127,10 +83,3 @@ if not database_exists(f'mysql+pymysql://{username}:{password}@{host}/off1'):
 # If the database already exist, then inform the user about it.
 else:
     print("Your database already exists.")
-
-    # NB ATTENTION: Improvement to provide here, for further development:
-    # here the databae's seed is made in a way, that the substitute follows the
-    # product. There for substitute is define by 1 out of 2.
-    # However if data seeding is organised differently,
-    # this script wouldn't work out. Something more automatic, more
-    # systematic, and accurate should be developped.
