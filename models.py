a635c7417a55d9dc5959f5a7480b88d5882c2c13
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-08-04 22:03:49 +0200

    PEP8 - models.py

--- models.py
+++ models.py
@@ -3,7 +3,8 @@
 # Date: Thu 11 Jul 2019 16:05:53 CEST
 # Author: Nicolas Flandrois
 
-from sqlalchemy import Column, Integer, Float, String, ForeignKey, DateTime, Boolean
+from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from connection import connect
 
@@ -19,7 +20,7 @@ class Pizza(Base):
     name = Column(String(50))
     price = Column(Float(4, decimal_return_scale=2))  # In local currency
     vat = Column(Integer, ForeignKey('vat.id'))
-    
+
 
 class Ingredient(Base):
     """docstring for Ingredient"""
@@ -39,6 +40,7 @@ class Ingredient(Base):
     # Purchasing cost in Delivery units (in local currency)
     cost = Column(Float(4, decimal_return_scale=2))
 
+
 class Recipe_ingredient(Base):
     """docstring for Recipe"""
     __tablename__ = "recipe_ingredient"
@@ -47,19 +49,22 @@ class Recipe_ingredient(Base):
     ingredient = Column(Integer, ForeignKey('ingredient.id'), nullable=False)
     quantity = Column(Float(4, decimal_return_scale=2))
 
+
 class Restaurant(Base):
     """docstring for Restaurant"""
     __tablename__ = "restaurant"
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
 
+
 class Stock(Base):
     """docstring for Stock"""
     __tablename__ = "stock"
     id = Column(Integer, primary_key=True)
-    stock_loc =  Column(Integer, ForeignKey('restaurant.id'), nullable=False)
+    stock_loc = Column(Integer, ForeignKey('restaurant.id'), nullable=False)
     ingredient = Column(Integer, ForeignKey('ingredient.id'), nullable=False)
     quantity = Column(Integer)
+
 
 class Payment_status(Base):
     """docstring for PaymentStatus"""
@@ -67,11 +72,13 @@ class Payment_status(Base):
     id = Column(Integer, primary_key=True)
     status = Column(String(50))
 
+
 class Order_status(Base):
     """docstring for Order Status"""
     __tablename__ = "order_status"
     id = Column(Integer, primary_key=True)
     status = Column(String(20))
+
 
 class Client(Base):
     """docstring for Client"""
@@ -82,6 +89,7 @@ class Client(Base):
     email = Column(String(50), nullable=False)
     phone = Column(String(10), nullable=False)
 
+
 class Address(Base):
     """docstring for Adresses"""
     __tablename__ = "address"
@@ -90,11 +98,13 @@ class Address(Base):
     address = Column(String(500), nullable=False)
     invoice = Column(Boolean, nullable=False)
 
+
 class Vat(Base):
     """docstring for Vat"""
     __tablename__ = "vat"
     id = Column(Integer, primary_key=True)
     rate = Column(Float(decimal_return_scale=4))
+
 
 class Order(Base):
     """docstring for Orders"""
@@ -102,8 +112,11 @@ class Order(Base):
     id = Column(Integer, primary_key=True)
     date = Column(DateTime, nullable=False)
     client = Column(Integer, ForeignKey('client.id'), nullable=False)
-    order_status = Column(Integer, ForeignKey('order_status.id'), nullable=False)
-    payment_status = Column(Integer, ForeignKey('payment_status.id'), nullable=False)
+    order_status = Column(Integer, ForeignKey('order_status.id'),
+                          nullable=False)
+    payment_status = Column(Integer, ForeignKey('payment_status.id'),
+                            nullable=False)
+
 
 class Pizza_ordered(Base):
     """docstring for Pizza_ordered"""
